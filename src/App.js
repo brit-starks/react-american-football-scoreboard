4cc77cbd36ff2371321974a09f4a5b06621ce347
brit-starks <brittneytechiethomas@gmail.com>
2019-07-24 20:04:54 -0700

    Complete functionality for buttons

--- src/App.js
+++ src/App.js
@@ -7,9 +7,8 @@ import {useState} from 'react';
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 
-    const [score, setScore] = useState(0);
-    const [awayScore, setAwayScore] = useState(0);
-
+    let [score, setScore] = useState(0);
+    let [awayScore, setAwayScore] = useState(0);
 
     return (
       <div className="container">
@@ -33,12 +32,12 @@ function App() {
         <section className="buttons">
           <div className="homeButtons">
             {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-            <button className="homeButtons__touchdown">Home Touchdown</button>
-            <button className="homeButtons__fieldGoal">Home Field Goal</button>
+            <button onClick={() => setScore(score+ 7)} className="homeButtons__touchdown">Home Touchdown</button>
+            <button onClick={() => setScore(score+ 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
           </div>
           <div className="awayButtons">
-            <button className="awayButtons__touchdown">Away Touchdown</button>
-            <button className="awayButtons__fieldGoal">Away Field Goal</button>
+            <button onClick={() => setAwayScore(awayScore+ 7)} className="awayButtons__touchdown">Away Touchdown</button>
+            <button onClick ={() => setAwayScore(awayScore+ 7)} className="awayButtons__fieldGoal">Away Field Goal</button>
           </div>
         </section>
       </div>
